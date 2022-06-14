0086ff97d97db3331d3c9028b4f8d9794bd7f1cc
ACER <ACER@LAPTOP-5FPFS2OH>
2022-06-14 15:35:30 +0700

    Add project files.

---
+++ LAB_5_TH/Controllers/RubikController.cs
@@ -0,0 +1,20 @@
+﻿using LAB_5_TH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB_5_TH.Controllers
+{
+    public class RubikController : Controller
+    {
+        // GET: Rubik
+        RubikStoreDataContext data = new RubikStoreDataContext();
+        public ActionResult Index()
+        {
+            var all_rubik = from s in data.Rubiks select s;
+            return View(all_rubik);
+        }
+    }
+}
